f346f2187ec489ea61c51d6e8b20b44323e36590
Arimlna <160694744+Arimlna@users.noreply.github.com>
2024-03-24 18:53:21 +0700

    Add files via upload

---
+++ kalklator.py
@@ -0,0 +1,8 @@
+inputan = input('Masukan nama :')
+print('Hallo Selamat Datang{Ari},selamat belajar {matakuliah}!')
+
+celcius = float(input("Masukan Suhu dalam celcius : "))
+print("suhu adalah :", celcius, "celcius")
+
+fahrenheit = (9 / 5 * celcius) + 32
+print("suhu dalam fahrenheit adalah :", fahrenheit, "fahrenheit")
